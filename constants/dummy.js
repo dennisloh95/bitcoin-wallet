d531395ae179dacbfb86e03f22243f629f17d678
Dennis Loh <dennis@Denniss-MacBook-Air.local>
2021-05-13 17:58:44 +0800

    remove dummy data

--- constants/dummy.js
+++ constants/dummy.js
@@ -1,31 +1,8 @@
-const chartOptions = [
-  {
-    id: 1,
-    label: "1 hr",
-  },
-  {
-    id: 2,
-    label: "3 Days",
-  },
-  {
-    id: 3,
-    label: "1 Week",
-  },
-  {
-    id: 4,
-    label: "1 Month",
-  },
-  {
-    id: 5,
-    label: "3 Months",
-  },
-];
-
 const api = {
   currentPrice: "https://api.coindesk.com/v1/bpi/currentprice.json",
   closePrice: "https://api.coindesk.com/v1/bpi/historical/close.json",
 };
 
-const dummyData = { chartOptions, api };
+const dummyData = { api };
 
 export default dummyData;
